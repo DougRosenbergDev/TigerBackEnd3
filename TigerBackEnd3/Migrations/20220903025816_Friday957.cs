300c891ef4495078a51edb53548cfe12c77d897a
dougrosenbergdev <drosenberg@skillstorm.com>
2022-09-02 22:00:27 -0500

    deleted weather

---
+++ TigerBackEnd3/Migrations/20220903025816_Friday957.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TigerBackEnd3.Migrations
+{
+    public partial class Friday957 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DeviceLimit",
+                table: "Plans",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PlanName",
+                table: "Plans",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PlanPrice",
+                table: "Plans",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Devices",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "DeviceLimit",
+                table: "Plans");
+
+            migrationBuilder.DropColumn(
+                name: "PlanName",
+                table: "Plans");
+
+            migrationBuilder.DropColumn(
+                name: "PlanPrice",
+                table: "Plans");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Devices");
+        }
+    }
+}
